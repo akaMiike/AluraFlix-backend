5ee98d786e79001aaa1d5ab4ab8f67b33cc7070f
Michael Nicholas <50562060+akaMiike@users.noreply.github.com>
2022-02-07 22:57:23 -0300

    Arquivos Iniciais do Spring

---
+++ aluraflix/src/main/kotlin/com/example/aluraflix/AluraflixBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.example.aluraflix
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AluraflixBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<AluraflixBackendApplication>(*args)
+}
---
+++ aluraflix/src/test/kotlin/com/example/aluraflix/AluraflixBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.aluraflix
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AluraflixBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
